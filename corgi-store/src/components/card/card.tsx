3b427f6fa005e6fbdfea6ea1182bc7c82add25bf
Muhtasim-Fuad-Showmik <muhtasimfuadshowmik@gmail.com>
2022-12-18 18:33:04 +0600

    Clicking on cards display corgi details

--- corgi-store/src/components/card/card.tsx
+++ corgi-store/src/components/card/card.tsx
@@ -6,7 +6,10 @@ export default component$((props) => {
 
 	return (
         <Link href="/corgi">
-            <div class="flex flex-col gap-2 cursor-pointer border border-solid border-transparent hover:border-slate-900">
+            <div class="flex flex-col gap-2 cursor-pointer border border-solid border-transparent hover:border-slate-900"
+            onClick$={() => {
+                localStorage.setItem('corgiData', JSON.stringify({...props}));
+            }}>
                 <img src={url} alt="corgi picture" class="sm:h-[300px] object-cover" />
                 <div class="flex flex-col gap-2 p-4 shadow">
                     <h2 class="text-2xl">{name}</h2>
--- corgi-store/src/routes/corgi/index.tsx
+++ corgi-store/src/routes/corgi/index.tsx
@@ -1,14 +1,31 @@
-import { component$ } from '@builder.io/qwik';
-import { DocumentHead } from '@builder.io/qwik-city';
+import { component$, useClientEffect$, useStore } from "@builder.io/qwik";
+import { DocumentHead, useLocation  } from "@builder.io/qwik-city";
 
 export default component$(() => {
-  return (
-    <>
-      <h1>Hello</h1>
-    </>
-  );
+	const loc = useLocation();
+
+	const state = useStore({
+		name: '',
+		price: '',
+		url: ''
+	});
+	
+	useClientEffect$(() => {
+		const data = JSON.parse(localStorage.getItem('corgiData'));
+		state.name = data.name;
+		state.url = data.url;
+		state.price = data.price;
+	});
+
+    return (
+        <>
+            <h1>
+				{state.name}
+			</h1>
+        </>
+    );
 });
 
 export const head: DocumentHead = {
-  title: 'Qwik Flower',
+    title: "Corgi Details",
 };
