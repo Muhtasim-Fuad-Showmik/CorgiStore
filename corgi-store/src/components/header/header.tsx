2d1d20c804de1cf55b880bd4334219de0cf49034
Muhtasim-Fuad-Showmik <muhtasimfuadshowmik@gmail.com>
2022-12-22 15:09:55 +0600

    created modals with passable functions

--- corgi-store/src/components/header/header.tsx
+++ corgi-store/src/components/header/header.tsx
@@ -1,9 +1,11 @@
-import { component$, useClientEffect$, useStore, useContext } from "@builder.io/qwik";
+import { component$, useClientEffect$, useStore, useContext, $ } from "@builder.io/qwik";
 import { strictEqual } from "assert";
 import { MyContext } from "~/root";
+import Modal from "../modal/modal";
 
 export default component$(() => {
 	const contextState = useContext(MyContext);
+	const onClose = $(() => store.modal = false);
 
 	const store = useStore({
 		scrolled: false,
@@ -50,33 +52,7 @@ export default component$(() => {
 					</div>
 				}
 			</div>
-			{store.modal && <>
-				<div class="absolute top-0 right-0 w-full h-screen bg-white z-50 flex flex-col gap-4 p-4 sm:w-[500px] text-slate-900">
-					<div class="flex items-center justify-between pb-4 border-b">
-						<h1 class="font-bold text-4xl">CART</h1>
-						<i onClick$={() => {
-							store.modal = false;
-						}} class="fa-solid fa-xmark cursor-pointer hover:rotate-45"></i>
-					</div>
-					{store.cart > 0 ?
-					<div class="bg-slate-400 flex flex-col gap-[1px]">
-						{store.cart.map((item) => {
-							return (
-								<div class="bg-white p-4 flex items-center justify-between">
-									<div class="flex flex-col gap-1">
-										<h1>{item.name}</h1>
-										<p>{item.price}</p>
-									</div>
-									<i class="fa-solid fa-xmark cursor-pointer"></i>
-								</div>
-							);
-						})}
-					</div> :
-					<div>
-						<h3 class="text-sm">Your Cart is Empty</h3>
-					</div>}
-				</div>
-			</>}
+			{store.modal && <Modal onClose={onClose} cart={store.cart}/>}
 		</header>
 	);
 });
